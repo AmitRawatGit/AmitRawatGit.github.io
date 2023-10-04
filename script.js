f47b3b3058596de2f52ec3884b59de004510f735
Amit Rawat <amit.rawat@3SCSOLUTION.LAN>
2023-10-04 12:55:45 +0530

    completed portfolio

---
+++ script.js
@@ -0,0 +1,102 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Python Developer", "Django Developer", "Software Engineer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Python Developer", "Django Developer", "Software Engineer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+    document.getElementById('downloadButton').addEventListener('click', function() {
+        const filename = 'Amit Rawat.pdf'; // Change to your resume file name and extension
+        const fileURL = 'documents/Amit_Rawat.pdf'; // Change to the actual path of your resume file
+      
+        const a = document.createElement('a');
+        a.href = fileURL;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      });
+});
+const yearLabel = document.getElementById('year');
+const currentYear = new Date().getFullYear();
+yearLabel.innerText = `${currentYear}`;
+
+function validateForm() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    if (name === '' || email === '' || subject === '' || message === '') {
+        alert('Please fill in all fields.');
+        return false;
+    }
+    document.getElementById('contactForm').reset();
+    event.preventDefault();
+    return true;
+}
